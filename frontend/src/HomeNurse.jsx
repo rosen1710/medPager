0c93b33cfd2af05021911cd2f656c8ebcd3241b6
martina <martina.n.bikova.2020@elsys-bg.org>
2025-03-21 18:39:00 +0200

    connected with the backend (works)

--- frontend/src/HomeNurse.jsx
+++ frontend/src/HomeNurse.jsx
@@ -1,31 +1,26 @@
 import axios from 'axios';
 
 export async function clientLoader() {
-    //const codes = await axios.get(`http://8888/emergencies`);
-    //return codes;
+    const codes = await axios.get(`http://192.168.5.86:5000/emergencies`);
+    return codes;
 }
 
-export default function HomeNurse({ }) {
-    const codes = {
-        "Black": "someone is armed and a threat",
-        "Blue": "life-threatening medical emergency",
-        "Brown": "external emergency",
-        "Gray": "unarmed but a threath",
-        "MET": "close to code blue",
-        "Orange": "evacuation",
-        "Pink": "unexpected labor unexpectedly or a newborn medical emergency",
-        "Purple": "bomb threat",
-        "Red": "fire",
-        "Yellow": "internal emergency"
-    };
+export async function clientAction() {
+    const formData = await request.formData();
+    const body = Object.fromEntries(formData);
+    const result = await axios.post(`http://192.168.5.86:5000/prompt`, body);
+}
+
+export default function HomeNurse({ loaderData }) {
+    const codes = loaderData.data;
 
     return(
         <div>
             <div>
                 <select name="code">
-                    {Object.keys(codes).map(code => {
+                    {Object.keys(codes).map(code =>         
                         <option value={code}>{code}</option>
-                    })}
+                    )}
                 </select>
                 <button>Page</button>
             </div>
