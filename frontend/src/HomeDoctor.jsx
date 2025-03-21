d296c98128379a1fa29545f9dd2d2de95ae89116
martina <martina.n.bikova.2020@elsys-bg.org>
2025-03-21 18:39:34 +0200

    connected with the backend (doens't work)

--- frontend/src/HomeDoctor.jsx
+++ frontend/src/HomeDoctor.jsx
@@ -1,61 +1,65 @@
 import axios from "axios";
 import { useState } from "react";
 
-export async function clientLoader() {
-    //const page = await axios.get(`http://localhost:2999/pages`);
+export async function clientLoader({params}) {
+    const { data: page } = await axios.get(`http://192.168.5.86:5000//get_page_by_doc/${params.id}`);
+    console.log("pages", page);
+    console.log([page, ...params.id]);
+    return {...page, ...params.id};
 }
 
-export default function HomeDoctor({  }) {
-    const pages = [
-        {
-            room_number: 202,
-            icd_code: "S82.001A",
-            description: "Fracture of patella, right knee, initial encounter"
-        },
-        {
-            room_number: 137,
-            icd_code: "S72.001A",
-            description: "Fracture of unspecified part of neck of right femur, initial encounter"
-        }
-        ];
+const Availability = async (available, id) => {
+    if(available) {
+        const result = await axios.post(`http://172.25.208.1:5000/available/${id}`);
+    }
+    else {
+        const result = await axios.post(`http://172.25.208.1:5000/unavailable/${id}`);
+    }
+}
 
-    const [isChecked, setIsChecked] = useState(false);
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked)
+export default function HomeDoctor({ loaderData }) {
+    console.log(loaderData);
+    const {pages, id} = loaderData;
+    console.log("pages", pages);
+    console.log("id", id);
+
+    const [available, setAvailable] = useState(false);
+    const handleAvailableChange = () => {
+        setAvailable(!available);
+        Availability(available, id);
     }
 
     return (
         <div>
-            <div>
+            <div className="toggle">
                 <p>Availability: </p>
                 <label className='flex cursor-pointer select-none items-center'>
                     <div className='relative'>
                         <input
                         type='checkbox'
-                        checked={isChecked}
-                        onChange={handleCheckboxChange}
+                        checked={available}
+                        onChange={handleAvailableChange}
                         className='sr-only'
                         />
                         <div
                         className={`box block h-8 w-14 rounded-full ${
-                            isChecked ? 'bg-primary' : 'bg-dark'
+                            available ? 'bg-primary' : 'bg-dark'
                         }`}
                         ></div>
                         <div
                         className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition ${
-                            isChecked ? 'translate-x-full' : ''
+                            available ? 'translate-x-full' : ''
                         }`}
                         ></div>
                     </div>
                 </label>
             </div>
             <ul>
-                {pages.map((page) => 
-                //TODO: VARIABLES NAMES MATCH
-                    <li className="page">
+                {pages.map(page => 
+                    <li className="doctors-page">
                         <p className="page-details">Room: {page.room_number}, {page.icd_code}</p>
-                        <p className="color: red">{page.description}</p>
                         <button className="delete-button">delete</button>
+                        <p className="page-desc">{page.symptoms}</p>
                     </li>
                 )}
             </ul>
