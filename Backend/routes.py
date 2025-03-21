ab10bb74ca7ed348e8dc30188ba0f8121cfbde33
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2025-03-21 14:56:53 +0200

    A

--- Backend/routes.py
+++ Backend/routes.py
@@ -1,14 +1,5 @@
-# / log in 
-# / log out 
-# + availability toggle
-# + заявка от frontend към backend за page с номер на стая и icd-10 код или описание на проблем
-# - заявка от backend към chat api за взимане на описание или icd-10 код 
 # - връщане на response от backend-a за frontend-a за допълване на page-a
 # - пращане на заявка от frontend към backend за пълен page
-# - пращане на page от backend към pager със стаята и icd-10 кода
-# - ще караме ли чата да мисли кой page за кое отделение е
-# - заявка от frontend към backend за взимане на page-ове (ти сортираш спрямо кое отделение са)
-# - изтриване на page
 
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
