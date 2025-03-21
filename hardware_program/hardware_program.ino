d380b05ea618084288a31818f9ca60a63a93e23a
inevg <33983752+inevg@users.noreply.github.com>
2025-03-21 12:58:01 +0200

    lcd

--- hardware_program/hardware_program.ino
+++ hardware_program/hardware_program.ino
@@ -1,4 +1,6 @@
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = "i0anbg";
 const char* password = "hiokay141414";
@@ -6,12 +8,19 @@ const char* host = "192.168.117.216";
 const uint16_t port = 12345;
 
 WiFiClient client;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+String lastMessage = "Waiting...";
 
 void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
   Serial.begin(115200);
+  
   WiFi.begin(ssid, password);
-
   Serial.print("Connecting to Wi-Fi");
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
@@ -36,14 +45,30 @@ void loop() {
     delay(5000);
     connectToServer();
   } else {
+    String response = "";
+    
     while (client.available()) {
-      String response = client.readStringUntil('\n');
+      response = client.readStringUntil('\n');
+      response.trim();
       Serial.print("Received: ");
       Serial.println(response);
+      
+      if (response.length() > 0) {
+        lastMessage = response;
+      }
     }
-    client.println("Ping!");
+
+    client.println("Ping!"); 
     delay(2000);
   }
+
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Message:");
+  lcd.setCursor(0, 1);
+  lcd.print(lastMessage);
+  delay(500);
 }
 
 void connectToServer() {
