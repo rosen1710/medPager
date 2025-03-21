2a0b339c443d9b0f740347cd37de919c306efb99
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2025-03-21 18:11:41 +0200

    Added more routes

--- Backend/routes.py
+++ Backend/routes.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from sqlalchemy import select
-from models import engine
 from sqlalchemy.orm import Session
-from models import Users, Pages
+from models import engine, Users, Pages, DoctorsDepartmentsMap
 from ai_connector import get_code_from_ai
 
 emergency_codes = {
@@ -22,11 +21,11 @@ emergency_codes = {
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/user/<doc_id>", methods=["GET"])
-def toggle(doc_id):
-    with Session(engine) as session:
-        user = session.query(Users).filter(Users.id == doc_id).first()
-    return make_response(user.name, 200)
+# @app.route("/user/<doc_id>", methods=["GET"])
+# def toggle(doc_id):
+#     with Session(engine) as session:
+#         user = session.query(Users).filter(Users.id == doc_id).first()
+#     return make_response(user.name, 200)
 
 @app.route("/unavailable/<doc_id>", methods=["POST"])
 def unavailable(doc_id):
@@ -56,22 +55,48 @@ def prompt():
     }
     return make_response(jsonify(response_data), 200)
 
-@app.route("/call", methods=["POST"])
-def call():
+@app.route("/create_page", methods=["POST"])
+def create_page():
     data = dict(request.json)
     with Session(engine) as session:
         new_page = Pages(
             room_number=data["room_number"],
             icd_code=data["icd_code"],
-            description=data["description"]
+            symptoms=data["symptoms"]
         )
         session.add(new_page) 
         session.commit()
     return make_response("Page successfully added", 200)
 
-@app.route("/emergencies", methods=["GET"])
-def emergencies():
-    return make_response(jsonify(emergency_codes), 200)
+@app.route("/get_pages", methods=["GET"])
+def get_all_pages():
+    with Session(engine) as session:
+        pages = session.query(Pages).all()
+        response_data = []
+        for page in pages:
+            response_data.append({
+                "id": page.id,
+                "room_number": page.room_number,
+                "icd_code": page.icd_code,
+                "symptoms": page.symptoms
+            })
+    return make_response(response_data, 200)
+
+@app.route("/get_page_by_doc/<doc_id>", methods=["GET"])
+def get_page(doc_id):
+    with Session(engine) as session:
+        doctor = session.query(DoctorsDepartmentsMap).filter(DoctorsDepartmentsMap.doctor == doc_id).first()
+        pages = session.query(Pages).filter(Pages.department == doctor.department).all()
+        response_data = []
+        print(pages)
+        for page in pages:
+            response_data.append({
+                "id": page.id,
+                "room_number": page.room_number,
+                "icd_code": page.icd_code,
+                "symptoms": page.symptoms
+            })
+    return make_response(response_data, 200)
 
 @app.route("/delete_page/<page_id>", methods=["DELETE"])
 def delete_page(page_id):
